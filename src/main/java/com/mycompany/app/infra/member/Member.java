262882f8dc8398ec21fcb1b63ee461cbb007d7e0
rainy2368 <rainy2368@gmail.com>
2023-07-05 17:43:50 +0900

    이게 맞나?

---
+++ src/main/java/com/mycompany/app/infra/member/Member.java
@@ -0,0 +1,59 @@
+package com.mycompany.app.infra.member;
+
+public class Member {
+	private String seq;
+	private String userID;
+	private String userPW;
+	private String userName;
+	private String userAge;
+	private String gender;
+	private String userStatus;
+
+	
+//	getter & setter
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPW() {
+		return userPW;
+	}
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(String userAge) {
+		this.userAge = userAge;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getUserStatus() {
+		return userStatus;
+	}
+	public void setUserStatus(String userStatus) {
+		this.userStatus = userStatus;
+	}
+	
+	
+}
