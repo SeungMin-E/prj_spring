5f65d7861224197048135a0fe2b76b5772dc9da2
rainy2368 <rainy2368@gmail.com>
2023-08-23 09:45:11 +0900

    업로드 손보는중 쉽지않네

--- src/main/java/com/mycompany/app/infra/memberUpload/Constants.java
+++ src/main/java/com/mycompany/app/infra/memberUpload/Constants.java
@@ -4,5 +4,6 @@ public class Constants{
 	public static final String UPLOAD_PATH_PREFIX_EXTERNAL = "D:/factory/ws_sts_4180_1/prj_spring/src/main/webapp";
 	public static final String UPLOAD_PATH_PREFIX = "D://factory/ws_sts_4180_1/prj_spring/src/main/webapp/resources/uploaded";
 	public static final String UPLOAD_PATH_PREFIX_FOR_VIEW = "/resources/uploaded";
+	public static final String DATA_TIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
 	public static final String DATETIME_FORMAT_BASIC =".jpg";
 }
